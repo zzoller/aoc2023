839ad880d678a6376bb5a3a344e5eee0b66cd6c2
Zac <zac@kivo.io>
2023-12-05 22:20:06 -0800

    day 6

---
+++ 6.ts
@@ -0,0 +1,38 @@
+import _ from "lodash";
+import { sample1, input } from "./input/6";
+
+const MPS = 1;
+
+const winnersPerTime = (time: number, bestDistance: number) => {
+  let numWaysToWin = 0;
+  _.range(time).map((t) => {
+    const speed = t * MPS;
+    const distance = (time - t) * speed;
+    if (distance > bestDistance) {
+      numWaysToWin++;
+    }
+  });
+  return numWaysToWin;
+};
+
+const parseLine = (line: string) => line.match(/\d+/g) as string[];
+
+const part1 = (input: string) => {
+  const lines = input.split("\n");
+  const times = parseLine(lines[0]).map((t) => parseInt(t));
+  const distances = parseLine(lines[1]).map((t) => parseInt(t));
+
+  return times
+    .map((t, i) => winnersPerTime(t, distances[i]))
+    .reduce((a, b) => a * b);
+};
+
+const part2 = (input: string) => {
+  const lines = input.split("\n");
+  const time = parseInt(parseLine(lines[0]).join(""));
+  const distance = parseInt(parseLine(lines[1]).join(""));
+  return winnersPerTime(time, distance);
+};
+
+console.log("part1", part1(input));
+console.log("part1", part2(input));
---
+++ input/6.ts
@@ -0,0 +1,5 @@
+export const sample1 = `Time:      7  15   30
+Distance:  9  40  200`;
+
+export const input = `Time:        49     87     78     95
+Distance:   356   1378   1502   1882`;
